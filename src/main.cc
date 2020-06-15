8e2a2472cd85fb246f83e8e3d04a41c3bdc37842
xuefeng <xuefeng1996@163.com>
2020-06-15 13:14:22 +0800

    main code

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
 #include <stdint.h>
 
-#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iostream>
 
 #include <Eigen/Core>
+#include <Eigen/Dense>
 
 #include <opencv2/opencv.hpp>
 
@@ -18,6 +20,8 @@ const bool kVisualize = false;
 const int kImageHeight = 192;
 const int kImageWidth  = 640;
 
+Params params;
+
 
 int main (int argc, char** argv) {
   if (argc<2) {
@@ -27,8 +31,12 @@ int main (int argc, char** argv) {
 
   std::string sequence_directory = argv[1];
 
+  std::ofstream outfile("result.txt", std::ios::app);
+  if (!outfile.is_open()) {
+    std::cerr << "Open file failed!" << std::endl;
+  }
+
   // set camera parameters
-  Params params;
   params.K.fx = 370.75;
   params.K.fy = 367.08;
   params.K.cu = 313.15;
@@ -36,9 +44,9 @@ int main (int argc, char** argv) {
   params.baseline = 0.54;
 
   // pose from camera to world
-  Eigen::Matrix<double,4,4> pose = Eigen::MatrixXd::Identity(4,4);
+  Eigen::Matrix4d pose = Eigen::MatrixXd::Identity(4,4);
 
-  for (int i = 0; i < 4541; i++) {
+  for (int i = 0; i < 3; i++) {
     // image file name
     char img_name[256]; sprintf(img_name, "%06d.png", i);
     std::string left_img_name  = sequence_directory + "/image_0/" + img_name;
@@ -55,81 +63,22 @@ int main (int argc, char** argv) {
     cv::resize(left_img,  left_img,  cv::Size(640, 192), cv::INTER_LINEAR);
     cv::resize(right_img, right_img, cv::Size(640, 192), cv::INTER_LINEAR);
 
+    Eigen::Matrix4d pose_inc;
+
     // entry to algorithm
-    ProcessFrame(left_img, right_img);
+    if (ProcessFrame(left_img, right_img, pose_inc)) {
+      pose = pose * pose_inc.inverse();
 
-
-
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+          outfile << pose(i, j) << " ";
+        }
+      }
+      outfile << std::endl;
+    }
   }
-  // // init visual odometry
-  // VisualOdometryStereo viso(param);
-  //   
-  // // loop through all frames i=0:372
-  // for (int32_t i=0; i<373; i++) {
 
-  //   // input file names
-  //   char base_name[256]; sprintf(base_name,"%06d.png",i);
-  //   string left_img_file_name  = dir + "/I1_" + base_name;
-  //   string right_img_file_name = dir + "/I2_" + base_name;
-  //   
-  //   // catch image read/write errors here
-  //   try {
+  outfile.close();
 
-  //     // load left and right input image
-  //     png::image< png::gray_pixel > left_img(left_img_file_name);
-  //     png::image< png::gray_pixel > right_img(right_img_file_name);
-
-  //     // image dimensions
-  //     int32_t width  = left_img.get_width();
-  //     int32_t height = left_img.get_height();
-
-  //     // convert input images to uint8_t buffer
-  //     uint8_t* left_img_data  = (uint8_t*)malloc(width*height*sizeof(uint8_t));
-  //     uint8_t* right_img_data = (uint8_t*)malloc(width*height*sizeof(uint8_t));
-  //     int32_t k=0;
-  //     for (int32_t v=0; v<height; v++) {
-  //       for (int32_t u=0; u<width; u++) {
-  //         left_img_data[k]  = left_img.get_pixel(u,v);
-  //         right_img_data[k] = right_img.get_pixel(u,v);
-  //         k++;
-  //       }
-  //     }
-
-  //     // status
-  //     cout << "Processing: Frame: " << i;
-  //     
-  //     // compute visual odometry
-  //     int32_t dims[] = {width,height,width};
-  //     if (viso.process(left_img_data,right_img_data,dims)) {
-  //     
-  //       // on success, update current pose
-  //       pose = pose * Matrix::inv(viso.getMotion());
-  //     
-  //       // output some statistics
-  //       double num_matches = viso.getNumberOfMatches();
-  //       double num_inliers = viso.getNumberOfInliers();
-  //       cout << ", Matches: " << num_matches;
-  //       cout << ", Inliers: " << 100.0*num_inliers/num_matches << " %" << ", Current pose: " << endl;
-  //       cout << pose << endl << endl;
-
-  //     } else {
-  //       cout << " ... failed!" << endl;
-  //     }
-
-  //     // release uint8_t buffers
-  //     free(left_img_data);
-  //     free(right_img_data);
-
-  //   // catch image read errors here
-  //   } catch (...) {
-  //     cerr << "ERROR: Couldn't read input files!" << endl;
-  //     return 1;
-  //   }
-  // }
-  
-  // // output
-  // cout << "Demo complete! Exiting ..." << endl;
-
-  // // exit
   return 0;
 }
